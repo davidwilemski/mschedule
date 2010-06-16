f5fa593db7936612099062a55c677d4171853dde
Bryan Kendall <bkend@Myrlin.local>
2010-06-16 16:46:59 -0400

    Changing up the controller and view again

---
+++ system/application/controllers/home.php
@@ -0,0 +1,23 @@
+<?php
+
+class Home extends Controller {
+	
+	function Home() {
+		
+		parent::controller();
+		
+	}
+	
+	function index() {		
+		
+		$data = array(
+			'view_name' => "home_view",
+			'ad'		=> "static/ads/google_ad_120_234.php"
+		);
+		$this->load->view('include/template', $data);
+		
+	}
+	
+}
+
+?>
