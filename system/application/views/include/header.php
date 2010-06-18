7f846ccfe2fb719264522ce807755d2d03a41222
Bryan Kendall <bkend@Myrlin.local>
2010-06-18 08:50:18 -0400

    Added the psd files and redoing the header

---
+++ system/application/views/include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>MSchedule</title>
+<?php echo $css; ?>
+
+<meta charset="UTF-8">
+</head>
+
+<body>
+
+<div id="title_pane">
+		<div id="title_name">
+			<?php echo anchor('home', img('static/images/mschedule_text.png')); ?>
+		</div>
+</div>
+
