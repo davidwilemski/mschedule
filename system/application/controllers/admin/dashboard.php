ae599065d7a0f1b481a25b2ca8115980015746e0
David Wilemski <dtwwtd@gmail.com>
2010-06-29 01:54:49 -0400

    Added layout to admin_dashboard and styling

--- system/application/controllers/admin/dashboard.php
+++ system/application/controllers/admin/dashboard.php
@@ -3,10 +3,19 @@
 		-secure- admin
 		This controller is going to be the main admin interface for updating
 		user information, static page content, and navigation links.
+		
+		This controller will handle the management of static pages and static page content as well as user profiles and 
+		will later be integrated with the mschedule app giving the ability to manage user schedules and manually update the
+		classes in our database.	
+		
 	*/
 ?>
 <?php
 
+		//This will be the management "dashboard" - show which pages have been recently updated and which users
+		//have recently logged in and which are recently registered.
+		//These will link to the pages that allow you to fully manage pages and users and so on.
+		
 class dashboard extends Controller {
 	
 	function dashboard() {
@@ -16,19 +25,21 @@ class dashboard extends Controller {
 		if(!$this->user_model->Secure(array('userType'=>'admin'))) {
 		
 			$this->session->set_flashdata('flashError', 'You must be logged in as an admin to access this page.');
-			$this->session->set_flashdata('flashRedirect', 'admin/dashboard');
+			$this->session->set_flashdata('flashRedirect', 'admin/dashboard'); //why is this set to admin/dashboard?
 			redirect('login');
 		
 		}
 		
  	}
 	
+	//Index page
+	//Should have panes 
 	function index() {
 		
 		$data = array(
 			'view_name'	=> 'admin/dashboard_view',
 			'navigation'=> "navigation",
-			'css'		=> includeCSSFile("style"),
+			'css'		=> includeCSSFile("style") . includeCSSFile("admin_dashboard"),
 			'nav_data'	=> $this->nav_links_model->getNavBarLinks()
 		);
 
--- system/application/views/admin/dashboard_view.php
+++ system/application/views/admin/dashboard_view.php
@@ -1,3 +1,37 @@
-<p><?php echo $this->session->userdata('username'); ?>
 
-Admin!</p>
+<div id="pane_left">
+	<h2>Changed Pages</h2>
+	<div class="pane">
+		<?php
+			for($i = 0; $i < 10; $i++)
+			{
+				echo "<p>Left $i</p>";
+			}
+		?>
+	</div>
+</div>
+
+<div id="pane_right">
+	<h2>New Users</h2>
+	<div class="pane">
+		<?php
+			for($i = 0; $i < 10; $i++)
+			{
+				echo "<p>Right $i</p>";
+			}
+		?>
+		
+	</div>
+</div>
+
+<div id="pane_center">
+	<h2>User Activity</h2>
+	<div class="pane">
+		<?php
+			for($i = 0; $i < 10; $i++)
+			{
+				echo "<p>Center $i</p>";
+			}
+		?>
+	</div>
+</div>
