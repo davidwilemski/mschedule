92ef16dc89dcba10831e663dcedd763af3935652
Bryan Kendall <bryan.a.kendall@gmail.com>
2010-12-29 14:05:36 -0500

    Schedule making function pushed to a seperate function. Put in previous/next schedule 'buttons' w/ checks. More awesome.

--- static/javascript/mschedule.js
+++ static/javascript/mschedule.js
@@ -286,169 +286,13 @@ $('document').ready(function () {
 			var json = jQuery.parseJSON(data);
 			
 			var schedule = json[0];
-			var time_denom = 30;
-			var box_per_hour = 60 / time_denom;
 			
-			var weekdays = Array(0,1,2,3,4,5,6);
+			var maxSchedules = json.length;
 			
-			var master = Array();
+			$("#schedule_div").html('<span id="put_table_here"></span>');
 			
-			for(var i = 0; i < 24; i++) {
-			
-				master[i] = Array();
-				
-				for(var j = 0; j < box_per_hour; j++) {
-				
-					master[i][j] = Array();
-					
-					for(var day in weekdays) {
-					
-						master[i][j][day] = '';
-					
-					}
-				
-				}
-			
-			}
-			
-			for(var c in schedule) {
-				
-				var days = schedule[c].days[0].split(',');
-				
-				for(var i = 0; i < days.length; i++) {
-				
-					var start_key = -1;
-					var start_key_minor = -1;
-					var end_key = -1;
-					var end_key_minor = -1;
-					
-					if(typeof days[i] != 'undefined')
-						var day = days[i];
-					else
-						var day = days[0];
-					
-					if(typeof schedule[c].time[i] != 'undefined')
-						var time = schedule[c].time[i];
-					else
-						var time = schedule[c].time[0];
-					time = time.split('-');
-					
-					// Work on the start time
-					if(time[0] % 100 == 0) {
-						// Then the start time is on the hour
-						start_key = time[0] / 100;
-						start_key_minor = 0;					
-					} else {
-						start_key_minor = 0;
-						while(time[0] % 100 != 0) {
-							time[0] -= time_denom;
-							start_key_minor++;
-						}
-						start_key = time[0] / 100;
-					}
-					
-					// Work on the end time
-					if(time[1] % 100 == 0) {
-						// Then the end time is on the hour
-						end_key = time[1] / 100;
-						end_key_minor = 0;					
-					} else {
-						end_key_minor = 0;
-						while(time[1] % 100 != 0) {
-							time[1] -= time_denom;
-							end_key_minor++;
-						}
-						end_key = time[1] / 100;
-					}
-					
-					var day_of_week = -1;
-					if(day == 'SU')
-						day_of_week = 0;
-					if(day == 'M')
-						day_of_week = 1;
-					if(day == 'TU')
-						day_of_week = 2;
-					if(day == 'W')
-						day_of_week = 3;
-					if(day == 'TH')
-						day_of_week = 4;
-					if(day == 'F')
-						day_of_week = 5;
-					if(day == 'SA')
-						day_of_week = 6;
-						
-					var begin = false;
-					for(start_key; start_key <= end_key; start_key++) {
-						if(!begin) {
-							for(start_key_minor; start_key_minor < box_per_hour; start_key_minor++) {
-								master[start_key][start_key_minor][day_of_week] = schedule[c].classid;
-							}
-							begin = true;
-						} else {
-							// If we are not quite to the end yet
-							if(start_key != end_key) {
-								for(var j = 0; j < box_per_hour; j++) {
-									master[start_key][j][day_of_week] = schedule[c].classid;
-								}
-							}
-							// If we are at the end
-							if(start_key == end_key) {
-								for(var j = 0; j < end_key_minor; j++) {
-									master[start_key][j][day_of_week] = schedule[c].classid;
-								}
-							}
-						}
-					}
-				
-				}
-			
-			}
-			
-			var day = new Date(2010, 1, 1, 0, 0, 0, 0);
-			
-			var td_id = 0;
-			// Number of columns: 7 (days of a week)
-			// Number of rows:    48 (24 hours, * 2)
-			var HTML_STRING = '';
-			HTML_STRING += '<table border="1">';
-			HTML_STRING += '<tbody>';
-			HTML_STRING += '<tr><td>Times</td><td>Sunday</td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td><td>Saturday</td></tr>';
-			for(var hour in master) {
-				for(var hour_part in master[hour]) {
-					var ROW_STRING = '';
-					ROW_STRING += '<td>';
-					var mins = day.getMinutes();
-					if(mins == "0")
-						mins = "00";
-					ROW_STRING += day.getHours() + ":" + mins;
-					//console.log(day);
-					ROW_STRING += '</td>';
-					var hide = true;
-					for(var weekday in master[hour][hour_part]) {
-						ROW_STRING += '<td id="' + td_id++ + '" ';
-						if(master[hour][hour_part][weekday] != '') { // We have a class here
-							hide = false;
-							ROW_STRING += 'class="visit" classid="' + master[hour][hour_part][weekday] + '"';
-						}
-						ROW_STRING += '>';
-						ROW_STRING += master[hour][hour_part][weekday];
-						ROW_STRING += '</td>';
-					}
-					if(hide && (day.getHours() < 8 || day.getHours() > 18)) // If we can hide this, and it's before 8 am or after 6 pm
-						HTML_STRING += '<tr class="hide">' + ROW_STRING + '</tr>';
-					else
-						HTML_STRING += '<tr>' + ROW_STRING + '</tr>';
-					
-					// Increment the day.
-					day.setMinutes(day.getMinutes() + time_denom);
-				}
-			}
-			HTML_STRING += '</tbody>';
-			HTML_STRING += '</table>';
-			
-			//console.log(HTML_STRING);
-			
-			var tableString = HTML_STRING;
+			// Function passing in a schedule to make the table
+			createWeekSchedule(schedule, 0, $("#put_table_here"));
 			
 			// This used to dump each schedule into the page in various tables. Lets not do that.
 			/*for(var j in json) {
@@ -492,16 +336,42 @@ $('document').ready(function () {
 				tableString += '</tbody></table>';
 			}*/
 			
-			// Add the table to the view
-			$("#schedule_div").html(tableString);
 			
-			// And hide the rows to be hidden
-			$('.hide').each(function() {
-				$(this).hide();
-			});
+	
+			// Add in the 'schedule id' that we worked with.
+			// This is for when we change what schedule we are looking at
+			// and which one we save.
+			$("#schedule_div").append('<span id="scheduleID" value="' + 0 + '" />');
 			
-			// combine the boxes that are the same classes
-			combBoxes();
+			// For now, let's also add a simple span to be able to go forward
+			// and backwards in the schedules
+			$("#schedule_div").append('<span id="next_schedule"><p>Next Schedule</p></span>');
+			$("#schedule_div").append('<span id="prev_schedule"><p>Prev Schedule</p></span>');
+			
+			// Hide the right buttons
+			hideNextPrev(0, maxSchedules);
+			
+			// Function for moving forward a schedule 
+			function nextButtonFunction() {
+				var next_index = 0;
+				next_index = $('#scheduleID').val() * 1 + 1;
+				$('#scheduleID').val(next_index);
+				$("#put_table_here").html("<p>Loading</p>");
+				createWeekSchedule(json[next_index], next_index, $('#put_table_here'));
+				hideNextPrev(next_index, maxSchedules);
+			}
+			$('#next_schedule').click(nextButtonFunction);
+			
+			// Function for moving back a schedule
+			function prevButtonFunction() {
+				var next_index = 0;
+				next_index = $('#scheduleID').val() * 1 - 1;
+				$('#scheduleID').val(next_index);
+				$("#put_table_here").html("<p>Loading</p>");
+				createWeekSchedule(json[next_index], next_index, $('#put_table_here'));
+				hideNextPrev(next_index, maxSchedules);
+			} 
+			$('#prev_schedule').click(prevButtonFunction);
 			
 			// Function for saving a schedule
 			$('.save_schedule').click(function() {
@@ -519,6 +389,192 @@ $('document').ready(function () {
 	});
 	
 });
+
+function hideNextPrev(myIndex, myMax) {
+	if(myIndex > 0 && myIndex < myMax) {
+		if($('#next_schedule').is(':hidden'))
+			$('#next_schedule').show();
+		if($('#prev_schedule').is(':hidden'))
+			$('#prev_schedule').show();
+	} 
+	if (myIndex <= 0)
+		$('#prev_schedule').hide();
+	if (myIndex >= myMax-1)
+		$('#next_schedule').hide();
+}
+
+function createWeekSchedule(schedule, myIndex, location) {
+	var time_denom = 30;
+	var box_per_hour = 60 / time_denom;
+	
+	var weekdays = Array(0,1,2,3,4,5,6);
+	
+	var master = Array();
+	
+	for(var i = 0; i < 24; i++) {
+	
+		master[i] = Array();
+		
+		for(var j = 0; j < box_per_hour; j++) {
+		
+			master[i][j] = Array();
+			
+			for(var day in weekdays) {
+			
+				master[i][j][day] = '';
+			
+			}
+		
+		}
+	
+	}
+	
+	for(var c in schedule) {
+		
+		var days = schedule[c].days[0].split(',');
+		
+		for(var i = 0; i < days.length; i++) {
+		
+			var start_key = -1;
+			var start_key_minor = -1;
+			var end_key = -1;
+			var end_key_minor = -1;
+			
+			if(typeof days[i] != 'undefined')
+				var day = days[i];
+			else
+				var day = days[0];
+			
+			if(typeof schedule[c].time[i] != 'undefined')
+				var time = schedule[c].time[i];
+			else
+				var time = schedule[c].time[0];
+			time = time.split('-');
+			
+			// Work on the start time
+			if(time[0] % 100 == 0) {
+				// Then the start time is on the hour
+				start_key = time[0] / 100;
+				start_key_minor = 0;					
+			} else {
+				start_key_minor = 0;
+				while(time[0] % 100 != 0) {
+					time[0] -= time_denom;
+					start_key_minor++;
+				}
+				start_key = time[0] / 100;
+			}
+			
+			// Work on the end time
+			if(time[1] % 100 == 0) {
+				// Then the end time is on the hour
+				end_key = time[1] / 100;
+				end_key_minor = 0;					
+			} else {
+				end_key_minor = 0;
+				while(time[1] % 100 != 0) {
+					time[1] -= time_denom;
+					end_key_minor++;
+				}
+				end_key = time[1] / 100;
+			}
+			
+			var day_of_week = -1;
+			if(day == 'SU')
+				day_of_week = 0;
+			if(day == 'M')
+				day_of_week = 1;
+			if(day == 'TU')
+				day_of_week = 2;
+			if(day == 'W')
+				day_of_week = 3;
+			if(day == 'TH')
+				day_of_week = 4;
+			if(day == 'F')
+				day_of_week = 5;
+			if(day == 'SA')
+				day_of_week = 6;
+				
+			var begin = false;
+			for(start_key; start_key <= end_key; start_key++) {
+				if(!begin) {
+					for(start_key_minor; start_key_minor < box_per_hour; start_key_minor++) {
+						master[start_key][start_key_minor][day_of_week] = schedule[c].classid;
+					}
+					begin = true;
+				} else {
+					// If we are not quite to the end yet
+					if(start_key != end_key) {
+						for(var j = 0; j < box_per_hour; j++) {
+							master[start_key][j][day_of_week] = schedule[c].classid;
+						}
+					}
+					// If we are at the end
+					if(start_key == end_key) {
+						for(var j = 0; j < end_key_minor; j++) {
+							master[start_key][j][day_of_week] = schedule[c].classid;
+						}
+					}
+				}
+			}
+		
+		}
+	
+	}
+	
+	var day = new Date(2010, 1, 1, 0, 0, 0, 0);
+	
+	var td_id = 0;
+	// Number of columns: 7 (days of a week)
+	// Number of rows:    48 (24 hours, * 2)
+	var HTML_STRING = '';
+	HTML_STRING += '<table border="1">';
+	HTML_STRING += '<tbody>';
+	HTML_STRING += '<tr><td>Times</td><td>Sunday</td><td>Monday</td><td>Tuesday</td><td>Wednesday</td><td>Thursday</td><td>Friday</td><td>Saturday</td></tr>';
+	for(var hour in master) {
+		for(var hour_part in master[hour]) {
+			var ROW_STRING = '';
+			ROW_STRING += '<td>';
+			var mins = day.getMinutes();
+			if(mins == "0")
+				mins = "00";
+			ROW_STRING += day.getHours() + ":" + mins;
+			//console.log(day);
+			ROW_STRING += '</td>';
+			var hide = true;
+			for(var weekday in master[hour][hour_part]) {
+				ROW_STRING += '<td id="' + td_id++ + '" ';
+				if(master[hour][hour_part][weekday] != '') { // We have a class here
+					hide = false;
+					ROW_STRING += 'class="visit" classid="' + master[hour][hour_part][weekday] + '"';
+				}
+				ROW_STRING += '>';
+				ROW_STRING += master[hour][hour_part][weekday];
+				ROW_STRING += '</td>';
+			}
+			if(hide && (day.getHours() < 8 || day.getHours() > 18)) // If we can hide this, and it's before 8 am or after 6 pm
+				HTML_STRING += '<tr class="hide">' + ROW_STRING + '</tr>';
+			else
+				HTML_STRING += '<tr>' + ROW_STRING + '</tr>';
+			
+			// Increment the day.
+			day.setMinutes(day.getMinutes() + time_denom);
+		}
+	}
+	HTML_STRING += '</tbody>';
+	HTML_STRING += '</table>';
+	
+	$(location).html(HTML_STRING);
+	
+	// And hide the rows to be hidden
+	$('.hide').each(function() {
+		$(this).hide();
+	});
+	
+	// combine the boxes that are the same classes
+	combBoxes();
+	
+}
 
 function combBoxes() {
 	while($('.visit').length > 0) {
