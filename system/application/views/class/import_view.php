599c0f0b71d698e9cdd43020204e2ffcd1e2c656
Bryan Kendall <bryan.a.kendall@gmail.com>
2010-12-31 02:35:45 -0500

    Hidden the table in the import classes view if no schedules have been created.

--- system/application/views/class/import_view.php
+++ system/application/views/class/import_view.php
@@ -3,30 +3,34 @@
 		This view is for loading in classes BY CLASS NUMBER.
 	*/
 ?>
-<table border=1>
-<?php
-echo '<tr>';
-echo '<td>Class ID</td>';
-echo '<td>Department</td>';
-echo '<td>Class Number</td>';
-echo '<td>Class Section</td>';
-echo '<td>Class Type</td>';
-echo '</tr>';
-
-$current_schedule_id = "";
-foreach($user_classes as $p) {
-	$current_schedule_id .= $p->classid . ";";
+<? if(is_array($user_classes)) { ?>
+	<table border=1>
+	<?php
 	echo '<tr>';
-	echo '<td>' . $p->classid . '</td>';
-	echo '<td>' . $p->dept . '</td>';
-	echo '<td>' . $p->number . '</td>';
-	echo '<td>' . $p->section . '</td>';
-	echo '<td>' . $p->type . '</td>';
+	echo '<td>Class ID</td>';
+	echo '<td>Department</td>';
+	echo '<td>Class Number</td>';
+	echo '<td>Class Section</td>';
+	echo '<td>Class Type</td>';
 	echo '</tr>';
+	
+	$current_schedule_id = "";
+	foreach($user_classes as $p) {
+		$current_schedule_id .= $p->classid . ";";
+		echo '<tr>';
+		echo '<td>' . $p->classid . '</td>';
+		echo '<td>' . $p->dept . '</td>';
+		echo '<td>' . $p->number . '</td>';
+		echo '<td>' . $p->section . '</td>';
+		echo '<td>' . $p->type . '</td>';
+		echo '</tr>';
+	}
+	?>
+	</table>
+<?
+} else {
+echo "<p>You have no classes yet!</p>";
 }
-?>
-</table>
-<?php 
 $this->load->helper('form');
 if($this->session->flashdata('error')) { ?>
 <div class="error"><?=$this->session->flashdata('error')?></div>
@@ -38,7 +42,7 @@ if(!$this->session->flashdata('fields')) $fields = '3'; else $fields = $this->se
 <fieldset>
 <legend>Import Classes</legend>
 <input type="hidden" id="class_boxes" name="class_boxes" value=<?=$fields?>></input>
-<input type="hidden" id="curr_schedule_string" name="curr_schedule_string" value=<?=$current_schedule_id?>></input>
+<input type="hidden" id="curr_schedule_string" name="curr_schedule_string" value="<?php if(isset($current_schedule_id)) echo $current_schedule_id; else echo ''; ?>"></input>
 <?php
 $inputs = array();
 for($i = 1; $i <=$fields; $i++) {
