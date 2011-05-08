ab90c7c380abea82d5bb0c255c47a67b3aa8846e
Ben Asher <basher@mobiata.com>
2011-05-08 15:29:52 -0400

    added Paul designer line to footer

--- system/application/views/include/footer.php
+++ system/application/views/include/footer.php
@@ -9,6 +9,7 @@
 	<div id="footer_content">
 		<p>For support, mschedule issues, free coffee, or mediocre advice, email webmaster [at] mschedule [dot] com.</p>
 		<p>Maintained by Bryan Kendall, David Wilemski, Tom Bombach, Ben Asher, and Jake Schwartz. &copy; Copyright <?php echo date('Y'); ?>.</p>
+		<p>Designed by Paul Provenza.</p>
 	</div>
 </div>
 
